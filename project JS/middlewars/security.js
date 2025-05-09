7c3fc0d4e1f6b199ba47f981e0fa37e5794be5f3
yasmine-jncj <hadriyassmine3@gmail.com>
2025-05-09 01:32:06 +0100

    Create security.js

---
+++ project JS/middlewars/security.js
@@ -0,0 +1,6 @@
+exports.securityHeaders = (req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    next();
+};
