92b5445a70401a79b2531521ba8f7cc40a3df4f0
yasmine-jncj <hadriyassmine3@gmail.com>
2025-05-09 01:39:16 +0100

    Update generatelink.js

--- project JS/utils/generatelink.js
+++ project JS/utils/generatelink.js
@@ -1,3 +1,15 @@
-// utils/generateLink.js
 const shortid = require('shortid');
-module.exports = () => `https://exam.com/${shortid.generate()}`;
+const config = require('../config');
+
+module.exports = {
+    generateExamLink: (examId) => {
+        const uniqueSlug = shortid.generate();
+        return {
+        slug: uniqueSlug,
+        fullUrl: `${config.BASE_URL}/exam/${uniqueSlug}/${examId}`
+        };
+    },
+    validateExamLink: (slug) => {
+        return shortid.isValid(slug) && slug.length >= 7 && slug.length <= 14;
+    }
+};
